8a332cde7b4cb2d3d2ea9468aa2486d969896dd9
Norayr Gharibyan <norayrgh@gmail.com>
2021-02-13 06:01:22 -0800

    done space move

--- src/main/java/Scroller.java
+++ src/main/java/Scroller.java
@@ -1,31 +1,27 @@
-import java.awt.MouseInfo;
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 
 public class Scroller extends Thread {
 
-  private final Robot robot;
+    private final Robot robot;
 
-  public Scroller(Robot robot) {
-    this.robot = robot;
-  }
-
-  @Override
-  public void run() {
-    while (!this.isInterrupted()) {
-      this.robot.keyPress(KeyEvent.VK_SPACE);
-      this.robot.mousePress(MouseEvent.BUTTON1);
-      this.robot.mouseMove(MouseInfo.getPointerInfo().getLocation().x+100, MouseInfo.getPointerInfo().getLocation().y);
-      try {
-        sleep(2000);
-      } catch (InterruptedException e) {
-        break;
-      }
-      this.robot.mouseMove(MouseInfo.getPointerInfo().getLocation().x-50, MouseInfo.getPointerInfo().getLocation().y);
-      this.robot.mouseRelease(MouseEvent.BUTTON1);
-      this.robot.keyRelease(KeyEvent.VK_SPACE);
+    public Scroller(Robot robot) {
+        this.robot = robot;
     }
-  }
+
+    @Override
+    public void run() {
+        while (!this.isInterrupted()) {
+            this.robot.mousePress(MouseEvent.BUTTON2_DOWN_MASK);
+            this.robot.mouseMove(MouseInfo.getPointerInfo().getLocation().x + 100, MouseInfo.getPointerInfo().getLocation().y);
+            try {
+                sleep(2000);
+            } catch (InterruptedException e) {
+                break;
+            }
+            this.robot.mouseMove(MouseInfo.getPointerInfo().getLocation().x - 100, MouseInfo.getPointerInfo().getLocation().y);
+            this.robot.mouseRelease(MouseEvent.BUTTON2_DOWN_MASK);
+        }
+    }
 
 }
