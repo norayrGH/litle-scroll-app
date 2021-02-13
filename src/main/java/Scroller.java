a26d73227ef382cae079eeaf6bb306d84aba667f
Norayr Gharibyan <norayrgh@gmail.com>
2021-02-13 10:58:51 -0800

    done space scroll.

--- src/main/java/Scroller.java
+++ src/main/java/Scroller.java
@@ -12,16 +12,21 @@ public class Scroller extends Thread {
     @Override
     public void run() {
         while (!this.isInterrupted()) {
-            this.robot.mousePress(MouseEvent.BUTTON2_DOWN_MASK);
-            this.robot.mouseMove(MouseInfo.getPointerInfo().getLocation().x + 100, MouseInfo.getPointerInfo().getLocation().y);
             try {
+                this.robot.mousePress(MouseEvent.BUTTON2_DOWN_MASK);
+                for (int i = 0; i < 10; i++) {
+                    this.robot.mouseMove(MouseInfo.getPointerInfo().getLocation().x + 5, MouseInfo.getPointerInfo().getLocation().y);
+                    sleep(200);
+                }
+                for (int i = 0; i < 10; i++) {
+                    this.robot.mouseMove(MouseInfo.getPointerInfo().getLocation().x - 5, MouseInfo.getPointerInfo().getLocation().y);
+                    sleep(200);
+                }
+                this.robot.mouseRelease(MouseEvent.BUTTON2_DOWN_MASK);
                 sleep(2000);
             } catch (InterruptedException e) {
                 break;
             }
-            this.robot.mouseMove(MouseInfo.getPointerInfo().getLocation().x - 100, MouseInfo.getPointerInfo().getLocation().y);
-            this.robot.mouseRelease(MouseEvent.BUTTON2_DOWN_MASK);
         }
     }
-
 }
